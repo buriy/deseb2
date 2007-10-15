7c7169bbf5f3225aaf02d25e586265b5f2d47528
public@kered.org <public@kered.org@64090aca-0e3b-0410-bb36-b14c205f76dc>
2007-10-15 18:34:34 +0000

    now supports sqlevolve for all apps (trunk only)
    git-svn-id: https://deseb.googlecode.com/svn/trunk@32 64090aca-0e3b-0410-bb36-b14c205f76dc

--- src/deseb/management/commands/sqlevolve.py
+++ src/deseb/management/commands/sqlevolve.py
@@ -7,6 +7,19 @@ See: http://code.google.com/p/deseb/wiki/Usage"""
 
     output_transaction = True
 
+    def handle(self, *app_labels, **options):
+        if not app_labels:
+            from django.db.models.loading import get_apps 
+            app_list = get_apps()
+            output = []
+            for app in app_list:
+                app_output = self.handle_app(app, **options)
+                if app_output:
+                    output.append(app_output)
+            return '\n'.join(output)
+        else:
+            super(Command, self).handle(*app_labels, **options)
+
     def handle_app(self, app, **options):
         import deseb.schema_evolution
         return '\n'.join(deseb.schema_evolution.get_sql_evolution(app, self.style))
