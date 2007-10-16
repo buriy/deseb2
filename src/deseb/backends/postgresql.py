4f5d89b21be73d922f6768c45aafc09aa60ca937
burchik <burchik@64090aca-0e3b-0410-bb36-b14c205f76dc>
2007-10-16 06:43:53 +0000

    Fixed issue 8: Double-quotes in postgres should be used for column names only, not in default values. http://code.google.com/p/deseb/issues/detail?id=8
    git-svn-id: https://deseb.googlecode.com/svn/trunk@38 64090aca-0e3b-0410-bb36-b14c205f76dc

--- src/deseb/backends/postgresql.py
+++ src/deseb/backends/postgresql.py
@@ -4,6 +4,14 @@ except NameError: from sets import Set as set   # Python 2.3 fallback
 
 
 class DatabaseOperations:
+    def quote_value(self, s):
+        if type(s) is bool:
+            if s: return "'t'"
+            else: return "'f'"
+        if type(s) is int:
+            return str(s)
+        else:
+            return u"'%s'" % unicode(s).replace("'","\'")
     
     def __init__(self, connection, style):
         self.connection = connection
@@ -13,49 +21,122 @@ class DatabaseOperations:
 
     def get_change_table_name_sql( self, table_name, old_table_name ):
         output = []
-        output.append(self.style.SQL_KEYWORD('ALTER TABLE ')+ self.style.SQL_TABLE(self.connection.ops.quote_name(old_table_name)) +self.style.SQL_KEYWORD(' RENAME TO ')+ self.style.SQL_TABLE(self.connection.ops.quote_name(table_name)) + ';')
+        qn = self.connection.ops.quote_name
+        output.append(
+            self.style.SQL_KEYWORD('ALTER TABLE ') + \
+            self.style.SQL_TABLE(qn(old_table_name)) + \
+            self.style.SQL_KEYWORD(' RENAME TO ') + \
+            self.style.SQL_TABLE(qn(table_name)) + ';')
         return output
     
     def get_change_column_name_sql( self, table_name, indexes, old_col_name, new_col_name, col_type, f ):
         # TODO: only supports a single primary key so far
         pk_name = None
+        qn = self.connection.ops.quote_name
         for key in indexes.keys():
             if indexes[key]['primary_key']: pk_name = key
         output = []
-        output.append( self.style.SQL_KEYWORD('ALTER TABLE ')+ self.style.SQL_TABLE(self.connection.ops.quote_name(table_name)) 
-                       +self.style.SQL_KEYWORD(' RENAME COLUMN ')+ self.style.SQL_FIELD(self.connection.ops.quote_name(old_col_name)) 
-                       +self.style.SQL_KEYWORD(' TO ')+ self.style.SQL_FIELD(self.connection.ops.quote_name(new_col_name)) +';' )
+        output.append( self.style.SQL_KEYWORD('ALTER TABLE ') + \
+            self.style.SQL_TABLE(qn(table_name)) + \
+            self.style.SQL_KEYWORD(' RENAME COLUMN ') + \
+            self.style.SQL_FIELD(qn(old_col_name)) + \
+            self.style.SQL_KEYWORD(' TO ') + \
+            self.style.SQL_FIELD(qn(new_col_name)) + ';' )
         return output
     
     def get_change_column_def_sql( self, table_name, col_name, col_type, f, column_flags ):
         output = []
-        output.append( self.style.SQL_KEYWORD('ALTER TABLE ')+ self.style.SQL_TABLE(self.connection.ops.quote_name(table_name)) +self.style.SQL_KEYWORD(' ADD COLUMN ')+ self.style.SQL_FIELD(self.connection.ops.quote_name(col_name+'_tmp')) +' '+ self.style.SQL_KEYWORD(col_type) + ';' )
-        output.append( self.style.SQL_KEYWORD('UPDATE ')+ self.style.SQL_TABLE(self.connection.ops.quote_name(table_name)) +self.style.SQL_KEYWORD(' SET ')+ self.style.SQL_FIELD(self.connection.ops.quote_name(col_name+'_tmp')) +' = '+ self.style.SQL_FIELD(self.connection.ops.quote_name(col_name)) + ';' )
-        output.append( self.style.SQL_KEYWORD('ALTER TABLE ')+ self.style.SQL_TABLE(self.connection.ops.quote_name(table_name)) +self.style.SQL_KEYWORD(' DROP COLUMN ')+ self.style.SQL_FIELD(self.connection.ops.quote_name(col_name)) +';' )
-        output.append( self.style.SQL_KEYWORD('ALTER TABLE ')+ self.style.SQL_TABLE(self.connection.ops.quote_name(table_name)) +self.style.SQL_KEYWORD(' RENAME COLUMN ')+ self.style.SQL_FIELD(self.connection.ops.quote_name(col_name+'_tmp')) +self.style.SQL_KEYWORD(' TO ')+ self.style.SQL_FIELD(self.connection.ops.quote_name(col_name)) + ';' )
+        qn = self.connection.ops.quote_name
+        output.append( 
+            self.style.SQL_KEYWORD('ALTER TABLE ') + \
+            self.style.SQL_TABLE(qn(table_name)) + \
+            self.style.SQL_KEYWORD(' ADD COLUMN ') + \
+            self.style.SQL_FIELD(qn(col_name+'_tmp')) + \
+            ' ' + self.style.SQL_KEYWORD(col_type) + ';' )
+        output.append( 
+            self.style.SQL_KEYWORD('UPDATE ') + \
+            self.style.SQL_TABLE(qn(table_name)) + \
+            self.style.SQL_KEYWORD(' SET ') + \
+            self.style.SQL_FIELD(qn(col_name+'_tmp')) + ' = ' + \
+            self.style.SQL_FIELD(qn(col_name)) + ';' )
+        output.append( self.style.SQL_KEYWORD('ALTER TABLE ') + \
+            self.style.SQL_TABLE(qn(table_name)) + \
+            self.style.SQL_KEYWORD(' DROP COLUMN ') + \
+            self.style.SQL_FIELD(qn(col_name)) + ';' )
+        output.append( self.style.SQL_KEYWORD('ALTER TABLE ') + \
+            self.style.SQL_TABLE(qn(table_name)) + \
+            self.style.SQL_KEYWORD(' RENAME COLUMN ') + \
+            self.style.SQL_FIELD(qn(col_name+'_tmp')) + \
+            self.style.SQL_KEYWORD(' TO ') + \
+            self.style.SQL_FIELD(qn(col_name)) + ';' )
         if f.default!=None and str(f.default) != 'django.db.models.fields.NOT_PROVIDED':
-            output.append( self.style.SQL_KEYWORD('ALTER TABLE ')+ self.style.SQL_TABLE(self.connection.ops.quote_name(table_name)) +self.style.SQL_KEYWORD(' ALTER COLUMN ')+ self.style.SQL_FIELD(self.connection.ops.quote_name(col_name)) +self.style.SQL_KEYWORD(' SET DEFAULT ')+ self.style.SQL_FIELD(self.connection.ops.quote_name(str(f.default))) +';' )
+            output.append( 
+                self.style.SQL_KEYWORD('ALTER TABLE ') + \
+                self.style.SQL_TABLE(qn(table_name)) + \
+                self.style.SQL_KEYWORD(' ALTER COLUMN ') + \
+                self.style.SQL_FIELD(qn(col_name)) + \
+                self.style.SQL_KEYWORD(' SET DEFAULT ') + \
+                self.style.SQL_FIELD(self.quote_value(f.default)) + ';' )
         if not f.null:
-            output.append( self.style.SQL_KEYWORD('ALTER TABLE ')+ self.style.SQL_TABLE(self.connection.ops.quote_name(table_name)) +self.style.SQL_KEYWORD(' ALTER COLUMN ')+ self.style.SQL_FIELD(self.connection.ops.quote_name(col_name)) +self.style.SQL_KEYWORD(' SET NOT NULL;') )
+            output.append( 
+                self.style.SQL_KEYWORD('ALTER TABLE ') + \
+                self.style.SQL_TABLE(qn(table_name)) + \
+                self.style.SQL_KEYWORD(' ALTER COLUMN ') + \
+                self.style.SQL_FIELD(qn(col_name)) + \
+                self.style.SQL_KEYWORD(' SET NOT NULL;') )
         if f.unique:
-            output.append( self.style.SQL_KEYWORD('ALTER TABLE ')+ self.style.SQL_TABLE(self.connection.ops.quote_name(table_name)) +self.style.SQL_KEYWORD(' ADD CONSTRAINT ')+ table_name +'_'+ col_name +'_unique_constraint'+self.style.SQL_KEYWORD(' UNIQUE(')+ self.style.SQL_FIELD(col_name) +self.style.SQL_KEYWORD(')')+';' )
+            output.append( self.style.SQL_KEYWORD('ALTER TABLE ') + \
+                self.style.SQL_TABLE(qn(table_name)) + \
+                self.style.SQL_KEYWORD(' ADD CONSTRAINT ') + \
+                table_name + '_' + col_name + '_unique_constraint'+ \
+                self.style.SQL_KEYWORD(' UNIQUE(') + \
+                self.style.SQL_FIELD(col_name) + \
+                self.style.SQL_KEYWORD(')')+';' )
         
         return output
     
     def get_add_column_sql( self, table_name, col_name, col_type, null, unique, primary_key, default ):
         output = []
-        output.append( self.style.SQL_KEYWORD('ALTER TABLE ')+ self.style.SQL_TABLE(self.connection.ops.quote_name(table_name)) +self.style.SQL_KEYWORD(' ADD COLUMN ')+ self.style.SQL_FIELD(self.connection.ops.quote_name(col_name)) +' '+ self.style.SQL_KEYWORD(col_type) + ';' )
+        qn = self.connection.ops.quote_name
+        output.append( 
+            self.style.SQL_KEYWORD('ALTER TABLE ') + \
+            self.style.SQL_TABLE(qn(table_name)) + \
+            self.style.SQL_KEYWORD(' ADD COLUMN ') + \
+            self.style.SQL_FIELD(qn(col_name)) + ' ' + \
+            self.style.SQL_KEYWORD(col_type) + ';' )
         if default!=None and str(default) != 'django.db.models.fields.NOT_PROVIDED':
-            output.append( self.style.SQL_KEYWORD('ALTER TABLE ')+ self.style.SQL_TABLE(self.connection.ops.quote_name(table_name)) +self.style.SQL_KEYWORD(' ALTER COLUMN ')+ self.style.SQL_FIELD(self.connection.ops.quote_name(col_name)) +self.style.SQL_KEYWORD(' SET DEFAULT ')+ self.connection.ops.quote_name(str(default)) +';' )
+            output.append( 
+                self.style.SQL_KEYWORD('ALTER TABLE ') + \
+                self.style.SQL_TABLE(qn(table_name)) + \
+                self.style.SQL_KEYWORD(' ALTER COLUMN ') + \
+                self.style.SQL_FIELD(qn(col_name)) + \
+                self.style.SQL_KEYWORD(' SET DEFAULT ') + \
+                self.style.SQL_FIELD(self.quote_value(f.default)) + ';' )
         if not null:
-            output.append( self.style.SQL_KEYWORD('ALTER TABLE ')+ self.style.SQL_TABLE(self.connection.ops.quote_name(table_name)) +self.style.SQL_KEYWORD(' ALTER COLUMN ')+ self.style.SQL_FIELD(self.connection.ops.quote_name(col_name)) +self.style.SQL_KEYWORD(' SET NOT NULL;') )
+            output.append( 
+                self.style.SQL_KEYWORD('ALTER TABLE ') + \
+                self.style.SQL_TABLE(qn(table_name)) + \
+                self.style.SQL_KEYWORD(' ALTER COLUMN ') + \
+                self.style.SQL_FIELD(qn(col_name)) + \
+                self.style.SQL_KEYWORD(' SET NOT NULL;') )
         if unique:
-            output.append( self.style.SQL_KEYWORD('ALTER TABLE ')+ self.style.SQL_TABLE(self.connection.ops.quote_name(table_name)) +self.style.SQL_KEYWORD(' ADD CONSTRAINT ')+ table_name +'_'+ col_name +'_unique_constraint'+self.style.SQL_KEYWORD(' UNIQUE(')+ self.style.SQL_FIELD(col_name) +self.style.SQL_KEYWORD(')')+';' )
+            output.append( 
+                self.style.SQL_KEYWORD('ALTER TABLE ') + \
+                self.style.SQL_TABLE(qn(table_name)) + \
+                self.style.SQL_KEYWORD(' ADD CONSTRAINT ') + \
+                table_name + '_' + col_name + '_unique_constraint'+ \
+                self.style.SQL_KEYWORD(' UNIQUE(') + \
+                self.style.SQL_FIELD(col_name) + \
+                self.style.SQL_KEYWORD(')')+';' )
         return output
     
     def get_drop_column_sql( self, table_name, col_name ):
         output = []
-        output.append( self.style.SQL_KEYWORD('ALTER TABLE ')+ self.style.SQL_TABLE(self.connection.ops.quote_name(table_name)) +self.style.SQL_KEYWORD(' DROP COLUMN ')+ self.style.SQL_FIELD(self.connection.ops.quote_name(col_name)) + ';' )
+        output.append( 
+            self.style.SQL_KEYWORD('ALTER TABLE ') + \
+            self.style.SQL_TABLE(qn(table_name)) + \
+            self.style.SQL_KEYWORD(' DROP COLUMN ') + \
+            self.style.SQL_FIELD(qn(col_name)) + ';' )
         return output
     
     def get_drop_table_sql( self, delete_tables):
